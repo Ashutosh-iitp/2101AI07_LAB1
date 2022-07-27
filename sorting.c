c247ceb96518a45d482e2b9258bb3977b99e1bea
ashutosh <ashutosh_2101ai07@iitp.ac.in>
2022-07-28 00:29:07 +0530

    code for insertion sort

--- sorting.c
+++ sorting.c
@@ -26,3 +26,20 @@ void Bubble(int n,int arr[])
 
             }
 }
+//Function Definiton for Insertion Sort
+void Insertion(int n,int arr[])
+{
+    int i,j,kys;
+    for (i = 1; i < n; i++)
+    {
+        kys = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > kys)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = kys;
+    }
+}
