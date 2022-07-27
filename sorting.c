bc40eed1db2417b6a586c8bf08671fc5d7887c02
ashutosh <ashutosh_2101ai07@iitp.ac.in>
2022-07-28 00:27:52 +0530

    code for bubble sort

--- sorting.c
+++ sorting.c
@@ -9,3 +9,20 @@ int swap(int *aj, int *bj)
 
     return 0;
 }
+//Function Definiton for Bubble Sort
+void Bubble(int n,int arr[])
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+
+        for (j = 0; j < n - i - 1; j++)
+
+            if (arr[j] > arr[j + 1])
+            {
+                //Algorithm for swapping two numbers
+                arr[j]=arr[j] - arr[j+1];
+                arr[j + 1]=arr[j] + arr[j + 1];
+                arr[j]=arr[j+1] - arr[j];
+
+            }
+}
