2e196946f90707f4ad474cfbb89c6b792f14ba9c
ashutosh <ashutosh_2101ai07@iitp.ac.in>
2022-07-27 23:52:32 +0530

    declaring variable and taking input from user

---
+++ calculator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+  char op;
+  double first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
