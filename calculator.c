cad652ccb14a80c735a1f391684c987f45d7e139
ashutosh <ashutosh_2101ai07@iitp.ac.in>
2022-07-27 23:56:16 +0530

    for * and / and finally printing result

--- calculator.c
+++ calculator.c
@@ -15,4 +15,17 @@ int main() {
       break;
       case '-':
         printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
-        break;  
+        break;
+        case '*':
+          printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+          break;
+        case '/':
+          printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+          break;
+        // operator doesn't match any case constant
+        default:
+          printf("Error! operator is not correct");
+      }
+
+      return 0;
+    }      
